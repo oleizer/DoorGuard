554d03615238c4f84be0ac29abd720e1f6e5dd95
Oleg L <oleg.l@smatym.pro>
2019-02-04 19:44:50 +0600

    Initial Commit

---
+++ DoorGuard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DoorGuard
+//
+//  Created by Олег Лейзер on 04/02/2019.
+//  Copyright © 2019 Олег Лейзер. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
